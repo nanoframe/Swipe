31e1ebd3cb4ad7f91da68666e9d91e3f09a207af
Vianney Nguyen <nguyenvianney@gmail.com>
2018-02-03 22:49:47 -0700

    Initial commit

---
+++ core/src/com/paperatus/swipe/Game.kt
@@ -0,0 +1,17 @@
+package com.paperatus.swipe
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.paperatus.swipe.scene.GameScene
+import com.paperatus.swipe.scene.SceneController
+
+class Game : ApplicationAdapter() {
+
+    override fun create() {
+    }
+
+    override fun render() {
+    }
+
+    override fun dispose() {
+    }
+}
