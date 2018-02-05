f1fec4758f1ace509bfd74fcff8659264f2f25f3
Vianney Nguyen <nguyenvianney@gmail.com>
2018-02-04 20:09:38 -0700

    Add base rendering code to the Scene class
    The Scene follows a design pattern that attempts to hide any references
    to the rendering code in inherited classes. An Array containing
    GameObjects is created to provide a way to render the objects upon
    calling the render(...) method on the Scene.
    
    Upon calling the render method, an operation is performed on all of the
    GameObjects with the following steps:
     - verify that the spriteName isn't blank and the image isn't null
     - determine the type of the image (Texture/TextureRegion)
     - render according to the properties defined in the GameObject

--- core/src/com/paperatus/swipe/scene/Scene.kt
+++ core/src/com/paperatus/swipe/scene/Scene.kt
@@ -1,15 +1,49 @@
 package com.paperatus.swipe.scene
 
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.utils.Disposable
+import com.paperatus.swipe.objects.GameObject
+import ktx.collections.GdxArray
 
 abstract class Scene : Disposable {
     lateinit var sceneController: SceneController
+    var gameObjects = GdxArray<GameObject>()
 
     abstract fun update(delta: Float)
     open fun render(batch: SpriteBatch) {
+        gameObjects.forEach {
+            assert(it.spriteName != "")
+            assert(sceneController.assets.isLoaded(it.spriteName))
 
+            val image: Any = sceneController.assets.get(it.spriteName)!!
+
+            when (image) {
+                is Texture -> batch.draw(image,
+                        it.position.x, it.position.y,
+                        0.5f, 0.5f,
+                        it.bounds.width, it.bounds.height,
+                        1.0f, 1.0f,
+                        it.rotation,
+                        0, 0, image.width, image.height,
+                        false, false
+                )
+                is TextureRegion -> batch.draw(image,
+                        it.position.x, it.position.y,
+                        0.5f, 0.5f,
+                        it.bounds.width, it.bounds.height,
+                        1.0f, 1.0f,
+                        it.rotation
+                )
+                else -> ktx.log.error(RuntimeException()) {
+                    "Asset '$it.spriteName' is of type ${image::class}!"
+                }
+            }
+        }
     }
 
     abstract fun reset()
+
+    inline fun<T : Any> GdxArray<T>.operate(action: GdxArray<T>.() -> Unit) = action()
 }
