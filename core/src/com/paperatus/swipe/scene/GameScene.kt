77d4b6942fd55fae7e2fb989fcecf9cf23163ef8
Vianney Nguyen <nguyenvianney@gmail.com>
2018-02-04 20:18:36 -0700

    Add GameScene

---
+++ core/src/com/paperatus/swipe/scene/GameScene.kt
@@ -0,0 +1,16 @@
+package com.paperatus.swipe.scene
+
+import com.paperatus.swipe.handlers.PlayerInput
+import com.paperatus.swipe.objects.Player
+
+class GameScene : Scene() {
+
+    override fun update(delta: Float) {
+    }
+
+    override fun reset() {
+    }
+
+    override fun dispose() {
+    }
+}
