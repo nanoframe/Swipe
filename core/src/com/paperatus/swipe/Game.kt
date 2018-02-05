5399a2911d23ea194aa8023467623cf7e390643c
Vianney Nguyen <nguyenvianney@gmail.com>
2018-02-04 20:23:38 -0700

    Update the SceneController every frame

--- core/src/com/paperatus/swipe/Game.kt
+++ core/src/com/paperatus/swipe/Game.kt
@@ -3,16 +3,28 @@ package com.paperatus.swipe
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.utils.ObjectMap
+import com.paperatus.swipe.scene.GameScene
+import com.paperatus.swipe.scene.SceneController
 
 class Game : ApplicationAdapter() {
 
+    private lateinit var sceneController: SceneController
+    private lateinit var assets: Assets
+
     override fun create() {
+        assets = Assets()
+
+        sceneController = SceneController(assets)
+        sceneController.addScene(GameScene())
+        sceneController.setScene<GameScene>()
     }
 
     override fun render() {
+        sceneController.step()
     }
 
     override fun dispose() {
+        sceneController.dispose()
     }
 }
 
