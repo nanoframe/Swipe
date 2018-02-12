77442032dc21d83fb1d10ae1f4b364c50c4252fb
Vianney Nguyen <nguyenvianney@gmail.com>
2018-02-11 22:11:41 -0700

    Adjust desktop window size

--- desktop/src/com/paperatus/swipe/desktop/DesktopLauncher.java
+++ desktop/src/com/paperatus/swipe/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.paperatus.swipe.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 360;
+		config.height = 640;
 		new LwjglApplication(new Game(), config);
 	}
 }
