5b4d3e98641b8f60e49f14da5549b81ae4c29790
Vianney Nguyen <nguyenvianney@gmail.com>
2018-02-12 20:33:45 -0700

    Remove Viewport usage

--- core/src/com/paperatus/swipe/scenes/GameScene.kt
+++ core/src/com/paperatus/swipe/scenes/GameScene.kt
@@ -1,13 +1,8 @@
 package com.paperatus.swipe.scenes
 
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.Sprite
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.math.Vector2
-import com.badlogic.gdx.utils.Scaling
-import com.badlogic.gdx.utils.viewport.ScalingViewport
-
 import com.paperatus.swipe.Game
 import com.paperatus.swipe.components.PlayerPhysicsComponent
 import com.paperatus.swipe.components.TouchInputComponent
@@ -22,11 +17,6 @@ const val WORLD_SIZE = 50.0f // World height
 class GameScene(game: Game) : PhysicsScene(game, Vector2.Zero) {
 
     private val camera = OrthographicCamera(WORLD_SIZE, WORLD_SIZE)
-    private val viewport = ScalingViewport(
-            Scaling.fit,
-            0.0f,
-            0.0f,
-            camera)
 
     private val player: Player = Player()
 
@@ -67,8 +57,8 @@ class GameScene(game: Game) : PhysicsScene(game, Vector2.Zero) {
         val WORLD_WIDTH = (width.toFloat() / height.toFloat()) * WORLD_SIZE
         val WORLD_HEIGHT = WORLD_SIZE
 
-        viewport.setWorldSize(WORLD_WIDTH, WORLD_HEIGHT)
-        viewport.update(width, height)
+        camera.viewportWidth = WORLD_WIDTH
+        camera.viewportHeight = WORLD_HEIGHT
 
         debug { "World dimensions: ($WORLD_WIDTH, $WORLD_HEIGHT)" }
     }
