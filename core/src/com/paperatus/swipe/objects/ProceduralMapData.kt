14dd5147deaeb953bb17196d4fbbcb82b3f4a452
Vianney Nguyen <nguyenvianney@gmail.com>
2018-02-17 22:47:38 -0700

    Generate world body chunks

--- core/src/com/paperatus/swipe/objects/ProceduralMapData.kt
+++ core/src/com/paperatus/swipe/objects/ProceduralMapData.kt
@@ -4,6 +4,9 @@ import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.physics.box2d.Body
+import com.badlogic.gdx.physics.box2d.BodyDef
+import com.badlogic.gdx.physics.box2d.EdgeShape
 import com.badlogic.gdx.physics.box2d.World
 import ktx.collections.GdxArray
 import ktx.log.Logger
@@ -11,8 +14,8 @@ import ktx.log.Logger
 private const val CHUNK_SIZE = 70.0f
 private const val GENERATE_GAP = 30.0f
 
-private const val MIN_Y = 3.0f
-private const val MAX_Y = 20.0f
+private const val MIN_Y = 8.0f
+private const val MAX_Y = 40.0f
 
 class ProceduralMapData : MapData {
 
@@ -25,8 +28,12 @@ class ProceduralMapData : MapData {
 
     private val points = GdxArray<Point>()
 
+    private val leftChunks = GdxArray<Chunk>()
+    private val rightChunks = GdxArray<Chunk>()
+
     override fun create() {
-        points.add(Point.obtain().also { // Defaults to (0, 0)
+        points.add(Point.obtain().also {
+            // Defaults to (0, 0)
             recentPoint = it
         })
     }
@@ -40,21 +47,40 @@ class ProceduralMapData : MapData {
         if (currentChunk * CHUNK_SIZE - cameraTop < GENERATE_GAP) {
             currentChunk++
 
-            log.debug { "Creating chunk #$currentChunk"}
+            log.debug { "Creating chunk #$currentChunk" }
 
             val cameraLeft = -camera.viewportWidth / 2.0f
             val cameraRight = camera.viewportWidth / 2.0f
 
-            do { // Generate a bunch of points for the chunk
+            val chunkLeft = Chunk.obtain()
+            val chunkRight = Chunk.obtain()
+            val width = 10.0f
 
+            chunkLeft.addPoint(
+                    recentPoint.x - width / 2.0f,
+                    recentPoint.y)
+            chunkRight.addPoint(
+                    recentPoint.x + width / 2.0f,
+                    recentPoint.y
+            )
+
+            do { // Generate a bunch of points for the chunk
                 val point = createNextPoint(cameraLeft, cameraRight)
                 points.add(point)
 
+                // Generate the walls of the map
+                chunkLeft.addPoint(point.x - width / 2.0f, point.y)
+                chunkRight.addPoint(point.x + width / 2.0f, point.y)
+
                 recentPoint = point
 
-                log.debug { "  Point created: (${point.x}, ${point.y})" }
-
             } while (recentPoint.y < currentChunk * CHUNK_SIZE)
+
+            createBodyChunk(world, chunkLeft)
+            createBodyChunk(world, chunkRight, true)
+
+            leftChunks.add(chunkLeft)
+            rightChunks.add(chunkRight)
         }
     }
 
@@ -63,14 +89,37 @@ class ProceduralMapData : MapData {
         renderer.color = Color.RED
         renderer.begin(ShapeRenderer.ShapeType.Line)
         for (i in 1 until points.size) {
-            val p1 = points[i-1]
+            val p1 = points[i - 1]
             val p2 = points[i]
             renderer.line(p1.x, p1.y, p2.x, p2.y)
         }
         renderer.end()
     }
 
-    private fun createNextPoint(leftBound: Float, rightBound: Float) : Point {
+    private fun createBodyChunk(world: World, chunk: Chunk, reverse: Boolean = false): Body {
+        val bodyDef = BodyDef()
+        val body = world.createBody(bodyDef)
+        val shape = EdgeShape()
+
+        if (reverse) chunk.reverse()
+
+        for (i in 1 until chunk.size) {
+            val point1 = chunk[i - 1]
+            val point2 = chunk[i]
+
+            shape.set(
+                    point1.x, point1.y,
+                    point2.x, point2.y)
+
+            body.createFixture(shape, 0.0f)
+
+        }
+        shape.dispose()
+
+        return body
+    }
+
+    private fun createNextPoint(leftBound: Float, rightBound: Float): Point {
         // Distance between the current x-position and the left/right edge
         val leftDelta = leftBound - recentPoint.x
         val rightDelta = rightBound - recentPoint.x
@@ -86,4 +135,5 @@ class ProceduralMapData : MapData {
 
         return point
     }
+
 }
