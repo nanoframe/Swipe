bae99e4bf53601e496053ac93cc29b3db05a9abb
Vianney Nguyen <nguyenvianney@gmail.com>
2018-02-20 17:01:49 -0700

    Replace Point class with Vector2

--- core/src/com/paperatus/swipe/objects/ProceduralMapData.kt
+++ core/src/com/paperatus/swipe/objects/ProceduralMapData.kt
@@ -3,10 +3,12 @@ package com.paperatus.swipe.objects
 import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.Body
 import com.badlogic.gdx.physics.box2d.BodyDef
 import com.badlogic.gdx.physics.box2d.EdgeShape
 import com.badlogic.gdx.physics.box2d.World
+import com.paperatus.swipe.data.Vector2Pool
 import ktx.collections.lastIndex
 import ktx.log.Logger
 import kotlin.math.max
@@ -27,13 +29,13 @@ class ProceduralMapData : MapData() {
         private val log = Logger("ProceduralMapData")
     }
 
-    private lateinit var recentPoint: Point
+    private lateinit var recentPoint: Vector2
     private var currentChunk = 0
 
     private var mapLimit: Body? = null
 
     override fun create() {
-        recentPoint = Point.obtain()
+        recentPoint = Vector2Pool.obtain()
     }
 
     override fun update(world: World, camera: Camera) {
@@ -92,12 +94,14 @@ class ProceduralMapData : MapData() {
 
             do { // Generate points until the y threshold is reached
                 val point = createNextPoint(cameraLeft, cameraRight)
+                val normalDirection = Vector2Pool.obtain()
+                normalDirection.set(point.y - recentPoint.y, recentPoint.x - point.x)
 
                 // Generate the walls of the map
                 chunkLeft.addPoint(point.x - width / 2.0f, point.y)
                 chunkRight.addPoint(point.x + width / 2.0f, point.y)
 
-                Point.free(recentPoint)
+                Vector2Pool.free(recentPoint)
                 recentPoint = point
 
             } while (recentPoint.y < currentChunk * CHUNK_SIZE)
@@ -142,7 +146,7 @@ class ProceduralMapData : MapData() {
         chunk.body = body
     }
 
-    private fun createNextPoint(leftBound: Float, rightBound: Float): Point {
+    private fun createNextPoint(leftBound: Float, rightBound: Float): Vector2 {
         // Distance between the current x-position and the left/right edge
         val leftDelta = leftBound - recentPoint.x
         val rightDelta = rightBound - recentPoint.x
@@ -150,7 +154,7 @@ class ProceduralMapData : MapData() {
         val deltaY = MathUtils.random(MIN_Y, MAX_Y)
 
         // Create a new path point
-        val point = Point.obtain()
+        val point = Vector2Pool.obtain()
         point.set(
                 recentPoint.x + MathUtils.random(leftDelta, rightDelta),
                 recentPoint.y + deltaY
