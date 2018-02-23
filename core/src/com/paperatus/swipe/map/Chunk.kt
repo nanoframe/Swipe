59983fd129b482cb5d1734a4e70615e47a8ef00c
Vianney Nguyen <nguyenvianney@gmail.com>
2018-02-22 17:24:27 -0700

    Encapsulate Chunk data classes in file

--- core/src/com/paperatus/swipe/map/Chunk.kt
+++ core/src/com/paperatus/swipe/map/Chunk.kt
@@ -1,11 +1,12 @@
 package com.paperatus.swipe.map
 
+import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.Body
 import com.badlogic.gdx.utils.Pool
 import ktx.collections.GdxArray
 
 // TODO: Implement separate array class
-class Chunk private constructor() : GdxArray<ChunkPoint>(), Pool.Poolable {
+class Chunk private constructor() : GdxArray<Vector2>(), Pool.Poolable {
 
     companion object : Pool<Chunk>() {
         override fun newObject(): Chunk {
@@ -16,7 +17,7 @@ class Chunk private constructor() : GdxArray<ChunkPoint>(), Pool.Poolable {
     var body: Body? = null
 
     fun addPoint(x: Float, y: Float) {
-        add(ChunkPoint.obtain().also {
+        add(Vector2Pool.obtain().also {
             it.x = x
             it.y = y
         })
@@ -24,24 +25,15 @@ class Chunk private constructor() : GdxArray<ChunkPoint>(), Pool.Poolable {
 
     override fun reset() {
         forEach {
-            ChunkPoint.free(it)
+            Vector2Pool.free(it)
         }
 
         clear()
     }
 }
 
-class ChunkPoint private constructor(var x: Float = 0.0f,
-                                     var y: Float = 0.0f) : Pool.Poolable {
-
-    companion object : Pool<ChunkPoint>() {
-        override fun newObject(): ChunkPoint {
-            return ChunkPoint()
-        }
-    }
-
-    override fun reset() {
-        x = 0.0f
-        y = 0.0f
+private object Vector2Pool : Pool<Vector2>() {
+    override fun newObject(): Vector2 {
+        return Vector2()
     }
 }
