991e415a4de905a8741c89b51d9a67d573f043fc
Vianney Nguyen <nguyenvianney@gmail.com>
2018-02-24 22:23:12 -0700

    Combine left&right path type into one

--- core/src/com/paperatus/swipe/map/Path.kt
+++ core/src/com/paperatus/swipe/map/Path.kt
@@ -5,43 +5,58 @@ import ktx.collections.set
 
 object Path {
 
-    // TODO: Add probability
-
     private val types: Array<Type>
     private val availableTypes = GdxArray<Type>(5)
 
     private val pathMap = GdxMap<Type, Int>()
+
+    private var pathType = PathType(Type.Uninitialized, Direction.None)
 
     init {
         types = Type.values()
     }
 
     enum class Type {
-        CurveLeft, CurveRight,
+        Uninitialized,
+        SoftCurve,
+        HardCurve,
         //BackLeft, BackRight,
-        CurveUp;
+        Up;
+    }
+
+    enum class Direction {
+        Left, Right, None;
     }
 
     fun setTypeCount(t: Type, count: Int) {
         pathMap[t] = count
     }
 
-    fun random(): Type {
+    fun random(): PathType {
         if (availableTypes.size <= 0) {
             resetTypes()
         }
 
-        return availableTypes.removeIndex(
-                            MathUtils.random(0,
-                                             availableTypes.size - 1))
+        val type = availableTypes.removeIndex(
+                MathUtils.random(0, availableTypes.size - 1))
+        pathType.type = type
+        pathType.direction =
+                if (MathUtils.randomBoolean()) { Direction.Left }
+                else { Direction.Right }
+
+        return pathType
     }
 
     private fun resetTypes() {
-        for (i in 0..types.lastIndex) {
+        // Skip the uninitialized type
+        for (i in 1..types.lastIndex) {
             val type = types[i]
             val count = pathMap[type, 1]
 
             for (j in 1..count) availableTypes.add(type)
         }
     }
+
+    data class PathType(var type: Type = Type.Uninitialized,
+                        var direction: Direction = Direction.None)
 }
--- core/src/com/paperatus/swipe/map/ProceduralMapData.kt
+++ core/src/com/paperatus/swipe/map/ProceduralMapData.kt
@@ -6,14 +6,14 @@ import com.badlogic.gdx.math.MathUtils
 import com.paperatus.swipe.data.PathPoint
 import ktx.collections.GdxArray
 
-private const val CURVE_MIN_Y_DISTANCE = 8.0f
-private const val CURVE_MAX_Y_DISTANCE = 20.0f
-private const val CURVE_MIN_POINTS = 3
-private const val CURVE_MAX_POINTS = 8
+private const val SOFT_CURVE_MIN_Y_DISTANCE = 8.0f
+private const val SOFT_CURVE_MAX_Y_DISTANCE = 20.0f
+private const val SOFT_CURVE_MIN_POINTS = 3
+private const val SOFT_CURVE_MAX_POINTS = 8
 
-private const val CURVE_UP_MIN_POINTS = 2
-private const val CURVE_UP_MAX_POINTS = 4
-private const val CURVE_UP_MAX_X_DELTA = 3.0f
+private const val UP_MIN_POINTS = 2
+private const val UP_MAX_POINTS = 4
+private const val UP_MAX_X_DELTA = 3.0f
 
 class ProceduralMapData : MapData() {
     override var pathColor = Color(204.0f / 255.0f, 230.0f / 255.0f, 228.0f / 255.0f, 1.0f)
@@ -24,18 +24,21 @@ class ProceduralMapData : MapData() {
                                 start: PathPoint): GdxArray<PathPoint> {
         tempArray.clear()
 
-        val pathType = Path.random()
+        val (type, direction) = Path.random()
 
-        when (pathType) {
-            Path.Type.CurveLeft -> generateLeftCurve(leftBound, start)
-            Path.Type.CurveRight -> generateRightCurve(rightBound, start)
-            Path.Type.CurveUp -> generateCurveUp(leftBound, rightBound, start)
+        when (type) {
+            Path.Type.SoftCurve -> generateSoftCurve(
+                    leftBound, rightBound, direction, start)
+            Path.Type.Up -> generateUp(leftBound, rightBound, start)
+            else -> throw RuntimeException("Unspecified type: $type")
         }
 
         return tempArray
     }
 
-    private fun generateLeftCurve(leftBound: Float,
+    private fun generateSoftCurve(leftBound: Float,
+                                  rightBound: Float,
+                                  direction: Path.Direction,
                                   start: PathPoint) {
         val count = randomCurvePointCount()
 
@@ -44,13 +47,15 @@ class ProceduralMapData : MapData() {
             val nextPoint = PathPoint.obtain()
             nextPoint.set(previous)
 
-            val leftDelta = leftBound - previous.x
+            val delta = when(direction) {
+                Path.Direction.Left -> leftBound - previous.x
+                Path.Direction.Right -> rightBound - previous.x
+                else -> throw RuntimeException("Unspecified direction: $direction")
+            }
 
-            val offsetX = MathUtils.random(
-                    leftDelta / 2.0f, leftDelta / 5.0f
-            )
+            val offsetX = MathUtils.random(delta / 2.0f, delta / 5.0f)
             val offsetY = MathUtils.random(
-                    CURVE_MIN_Y_DISTANCE, CURVE_MAX_Y_DISTANCE
+                    SOFT_CURVE_MIN_Y_DISTANCE, SOFT_CURVE_MAX_Y_DISTANCE
             )
 
             nextPoint.add(offsetX, offsetY)
@@ -60,33 +65,9 @@ class ProceduralMapData : MapData() {
         }
     }
 
-    private fun generateRightCurve(rightBound: Float,
-                                   start: PathPoint) {
-        val count = randomCurvePointCount()
-
-        var previous = start
-        for (i in 1..count) {
-            val nextPoint = PathPoint.obtain()
-            nextPoint.set(previous)
-
-            val rightDelta = rightBound - previous.x
-
-            val offsetX = MathUtils.random(
-                    rightDelta / 5.0f, rightDelta / 2.0f
-            )
-            val offsetY = MathUtils.random(
-                    CURVE_MIN_Y_DISTANCE, CURVE_MAX_Y_DISTANCE
-            )
-
-            nextPoint.add(offsetX, offsetY)
-            tempArray.add(nextPoint)
-
-            previous = nextPoint
-        }
-    }
-
-    private fun generateCurveUp(leftBound: Float, rightBound: Float,
-                                start: PathPoint) {
+    private fun generateUp(leftBound: Float,
+                           rightBound: Float,
+                           start: PathPoint) {
         val count = randomUpPointCount()
 
         var previousPoint = start
@@ -99,13 +80,13 @@ class ProceduralMapData : MapData() {
 
             val offsetX = MathUtils.clamp(
                     MathUtils.random(leftDelta, rightDelta),
-                    -CURVE_UP_MAX_X_DELTA,
-                    CURVE_UP_MAX_X_DELTA
+                    -UP_MAX_X_DELTA,
+                    UP_MAX_X_DELTA
             )
 
             nextPoint.add(
                     offsetX,
-                    MathUtils.random(CURVE_MIN_Y_DISTANCE, CURVE_MAX_Y_DISTANCE)
+                    MathUtils.random(SOFT_CURVE_MIN_Y_DISTANCE, SOFT_CURVE_MAX_Y_DISTANCE)
             )
 
             tempArray.add(nextPoint)
@@ -114,9 +95,9 @@ class ProceduralMapData : MapData() {
     }
 
     private fun randomCurvePointCount() = MathUtils.random(
-            CURVE_MIN_POINTS, CURVE_MAX_POINTS)
+            SOFT_CURVE_MIN_POINTS, SOFT_CURVE_MAX_POINTS)
 
     private fun randomUpPointCount() = MathUtils.random(
-            CURVE_UP_MIN_POINTS, CURVE_UP_MAX_POINTS
+            UP_MIN_POINTS, UP_MAX_POINTS
     )
 }
