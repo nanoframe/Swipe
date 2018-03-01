15c8e7c03057a614cbf80483f707b2f5b225e596
Vianney Nguyen <nguyenvianney@gmail.com>
2018-02-28 19:44:11 -0700

    Attach the physics component to Blockade

--- core/src/com/paperatus/swipe/objects/Blockade.kt
+++ core/src/com/paperatus/swipe/objects/Blockade.kt
@@ -1,8 +1,14 @@
 package com.paperatus.swipe.objects
 
+import com.paperatus.swipe.components.BlockadePhysicsComponent
 import com.paperatus.swipe.core.GameObject
+import com.paperatus.swipe.core.PhysicsComponent
 
 class Blockade : GameObject() {
     var health = 100.0f
     var maxHealth = 100.0f
+
+    init {
+        attachComponent<PhysicsComponent>(BlockadePhysicsComponent())
+    }
 }
