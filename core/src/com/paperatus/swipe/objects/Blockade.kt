179a4240993ea8d373d7ec2a122990e0ace9e974
Vianney Nguyen <nguyenvianney@gmail.com>
2018-02-28 21:37:21 -0700

    Deduct the blockade's health upon impact

--- core/src/com/paperatus/swipe/objects/Blockade.kt
+++ core/src/com/paperatus/swipe/objects/Blockade.kt
@@ -1,15 +1,27 @@
 package com.paperatus.swipe.objects
 
+import com.badlogic.gdx.physics.box2d.Body
 import com.paperatus.swipe.components.BlockadePhysicsComponent
 import com.paperatus.swipe.core.GameObject
 import com.paperatus.swipe.core.PhysicsComponent
 
-class Blockade : GameObject("blockade.png") {
+private const val DAMAGE_PER_VELOCITY = 2.0f
+class Blockade : GameObject("blockade.png"), PhysicsComponent.ContactListener {
 
     var health = 100.0f
     var maxHealth = 100.0f
 
     init {
-        attachComponent<PhysicsComponent>(BlockadePhysicsComponent())
+        attachComponent<PhysicsComponent>(BlockadePhysicsComponent().apply {
+            addContactListener(this@Blockade)
+        })
+    }
+
+    override fun onContactBegin(other: Body) {
+        val velocity = other.linearVelocity.len()
+        health -= DAMAGE_PER_VELOCITY * velocity
+    }
+
+    override fun onContactEnd(other: Body) {
     }
 }
