7d9951248085f7bb0631fdfd5f41772cd35c92af
Vianney Nguyen <nguyenvianney@gmail.com>
2018-03-01 18:01:25 -0700

    Create filter functions for GdxArray

--- core/src/com/paperatus/swipe/core/Classes.kt
+++ core/src/com/paperatus/swipe/core/Classes.kt
@@ -1,8 +1,27 @@
 package com.paperatus.swipe.core
 
+import ktx.collections.GdxArray
+
 data class Size(var width: Float = 0.0f, var height: Float = 0.0f) {
     fun set(w: Float, h: Float) {
         width = w
         height = h
     }
+}
+
+private val temp = GdxArray<Any>()
+fun <T> GdxArray<T>.filterBy(condition: (it: T) -> Boolean): GdxArray<T> {
+    temp.clear()
+
+    forEach {
+        if (condition(it)) {
+            temp.add(it)
+        }
+    }
+    return temp as GdxArray<T>
+}
+
+inline fun <reified T> GdxArray<*>.filterByType(): GdxArray<T> {
+    val arr = filterBy { it is T }
+    return arr as GdxArray<T>
 }
