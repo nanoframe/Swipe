626cddb1b6bf1693c6c7c87a6a2d6dde3ee9e1c3
Vianney Nguyen <nguyenvianney@gmail.com>
2018-03-05 19:31:17 -0700

    Fix rendering anchor issues for TextureRegions

--- core/src/com/paperatus/swipe/core/ObjectScene.kt
+++ core/src/com/paperatus/swipe/core/ObjectScene.kt
@@ -93,7 +93,7 @@ abstract class ObjectScene(protected val game: Game) : Scene {
                 is TextureRegion -> batch.draw(image,
                         position.x - size.width * anchor.x,
                         position.y - size.height * anchor.y,
-                        0.5f, 0.5f,
+                        anchor.x * size.width, anchor.y * size.height,
                         size.width, size.height,
                         1.0f, 1.0f,
                         rotation
