40dd7a19279a97c3e5ddf8a2f111eb85198daaea
Vianney Nguyen <nguyenvianney@gmail.com>
2018-03-11 21:49:38 -0600

    Tweak particle settings

--- core/src/main/com/paperatus/swipe/objects/Player.kt
+++ core/src/main/com/paperatus/swipe/objects/Player.kt
@@ -4,8 +4,8 @@ import com.paperatus.swipe.core.GameObject
 import com.paperatus.swipe.core.PhysicsComponent
 
 private const val PARTICLE_INITIAL_FREQUENCY: Float = 1.0f // per second
-private const val PARTICLE_VELOCITY_MULTIPLIER: Float = 2.0f
-private const val PARTICLE_MAX_FREQUENCY: Float = 25.0f
+private const val PARTICLE_VELOCITY_MULTIPLIER: Float = 1.2f
+private const val PARTICLE_MAX_FREQUENCY: Float = 35.0f
 class Player : GameObject() {
 
     private var elapsed = 0.0f
